a21d628841fd93b3f4d3d7d1bdbaa359c9dc384e
Amy Davies <amyloudav@gmail.com>
2022-10-10 13:11:05 +0200

    dynamic routes created and functional

--- pages/index.js
+++ pages/index.js
@@ -24,8 +24,8 @@ export default function Home({ allPostsData }) {
           check out my projects on GitHub
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          (This is a sample website - you can build a site like this here{" "}
+          <a href="https://nextjs.org/learn"> Next.js tutorial</a>)
         </p>
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
